92759f2811578c728a3e123d20b67e81cbec998a
Jessica Hyde <jessie5252@gmail.com>
2016-11-13 17:00:37 -0800

    Format fix

--- python/add_to_index.py
+++ python/add_to_index.py
@@ -19,6 +19,17 @@ def add_to_index(index,keyword,url):
 	   	return
    index.append([keyword,[url]])
 
+''' no dup check
+def add_to_index(index,keyword,url):
+	for entry in index:
+		if entry[0] == keyword:
+			entry[1].append(url)
+			return
+	index.append([keyword,[url]])
+'''
+
+
+
 
 
 add_to_index(index,'udacity','http://udacity.com')
