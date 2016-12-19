4c40034aac8f0ac57d464840feee4809e0149484
Jessica Hyde <jessie5252@gmail.com>
2016-12-18 23:11:53 -0800

    Have a new plan, scratching this for now

--- games/ww2_v1.py
+++ games/ww2_v1.py
@@ -15,9 +15,35 @@ printColor.red("Japan")
 printColor.green("Germany")
 printColor.blue("Great Britain")
 
-user_country = raw_input('Enter a country: ').capitalize()
+with open('countries_data.json', 'r') as jf:
+    countries = json.load(jf)
 
-while True:
+def validity(keyword,valid_options):
+	while True:
+		if keyword.capitalize() in valid_options:
+			print random.choice(reaction_pos)
+			break
+		else:
+			print "Invalid option."
+			user_country = raw_input('Please try again: ')
+			continue
+
+def get_inputs():
+	user_country = (raw_input('Enter a country: ')).capitalize()
+	validity(user_country, countries)
+	user_toy1 = raw_input("Tell me %s, what is your favorite toy?: "% user_country) # use leader later
+	print random.choice(reaction_pos)
+	user_fear1 = raw_input("What is your worst fear?: ")
+	print random.choice(reaction_pos)
+	user_wish1 = raw_input("If you could have anything you wanted, what would you wish for?: ") 
+	countries[user_country]['toys'][1] = user_toy1
+	countries[user_country]['fears'][1] = user_fear1
+	countries[user_country]['wishes'][1] = user_wish1
+	print countries[user_country]
+
+get_inputs()
+
+'''	
 	if user_country.capitalize() in countries:
 		print random.choice(reaction_pos)
 		user_toy= raw_input("Tell me %s, what is your favorite toy?: "% user_country) # use leader later
@@ -30,9 +56,7 @@ while True:
 
 
 #functions = lose_toy, realize_fear,lose_friend, leader_dies, lose_wish
-# if empty list, print none
-# add greeting, facts
-
 penalties = ["all women die", "all men die", "all animals die", "all plants die"]
-
+# if empty list, print none
+'''
 
