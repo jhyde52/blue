258ca8a826204aeb072f2fba5986c832226a6041
Jessica Hyde <jessie5252@gmail.com>
2017-01-15 21:31:58 -0800

    challenge questions

---
+++ udacity/cs1/wolfgram.py
@@ -0,0 +1,12 @@
+# plots a cellular automaton shape
+
+
+from cellular_automaton import cellular_automaton
+
+
+def plot_cellular(string0, pattern, generations):
+    print string0
+    for i in range(1,generations):
+        print cellular_automaton(string0, pattern, i)
+
+plot_cellular('....................x....................', 126, 100)
