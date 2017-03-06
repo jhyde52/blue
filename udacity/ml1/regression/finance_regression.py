bfa6b068c71980fc4ea1af18f07c5e52d6b08073
jhyde52 <jessie5252@gmail.com>
2017-03-05 18:13:51 -0800

    test

--- udacity/ml1/regression/finance_regression.py
+++ udacity/ml1/regression/finance_regression.py
@@ -65,7 +65,7 @@ for feature, target in zip(feature_train, target_train):
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
 plt.scatter(feature_test[0], target_test[0], color=train_color, label="train")
 
-
+a
 
 
 ### draw the regression line, once it's coded
