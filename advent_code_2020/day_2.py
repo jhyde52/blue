6ff28732097d8802fbe53776e533f1d4c2c6b313
jhyde52 <jhyde52@users.noreply.github.com>
2020-12-04 19:46:08 -0800

    Add files via upload
    Day 2 AoC

---
+++ advent_code_2020/day_2.py
@@ -0,0 +1,26 @@
+# check if password follows policy in first column
+
+def check_count_in_range(password, letter, start, stop):
+    return password.count(letter) >= start and password.count(letter) <= stop
+
+
+def check_indexes(password, letter, start, stop):
+    return (password[start] == letter or password[stop] == letter) and (password[start] != password[stop])
+
+with open('input_2.txt') as file:
+    letters = [num.split(": ") for num in file]
+    part1_tally = 0
+    part2_tally = 0
+    for policy, password in letters:
+        length, letter = policy.split(" ")
+        start, stop = length.split("-")
+        start, stop = int(start), int(stop)
+
+        # part 1
+        if check_count_in_range(password, letter, start, stop): part1_tally += 1
+
+        # part 2
+        if check_indexes(password, letter, start - 1, stop - 1): part2_tally += 1
+
+print("Part 1", part1_tally)
+print("Part 2", part2_tally)
